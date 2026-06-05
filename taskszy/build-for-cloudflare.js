ba64216fae2f43411304e4403676d73f122987d1
Aditya Raizada <mr.aaraizada@gmail.com>
2026-06-06 01:56:53 +0530

    commit
    commit

--- taskszy/build-for-cloudflare.js
+++ taskszy/build-for-cloudflare.js
@@ -1,6 +1,10 @@
-const fs = require('fs');
-const { execSync } = require('child_process');
-const path = require('path');
+import fs from 'fs';
+import { execSync } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 console.log('🚀 Starting TasksZy build process...\n');
 
